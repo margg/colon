1ee66a4c7c2d103e9b9f2d7fb314f2f654c52cba
marg <mksalawa@gmail.com>
2015-12-12 13:49:27 +0100

    logging for the win

--- colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
@@ -1,11 +1,17 @@
 package pl.edu.agh.to.testerka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static spark.Spark.get;
 
 public class Hello {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
+
     public static void main(String[] args) {
         get("/", (req, res) -> {
+            LOGGER.info("Received GET request.");
             return "hello from sparkjava.com";
         });
     }
