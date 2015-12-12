e765ac636335b266e7e8677a678eddabfeb81cda
agata <a.strojewska@gmail.com>
2015-12-12 15:22:13 +0100

    FileProvider (pliker) and RequestProvider classes added

--- colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
@@ -1,21 +1,18 @@
 package pl.edu.agh.to.testerka;
 
-import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static spark.Spark.get;
 
 public class Hello {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
 
     public static void main(String[] args) {
-        get("/solutions/:solution_id", (req, res) -> {
-            LOGGER.info("Received GET request for solution {}.", req.params(":solution_id"));
-            JsonObject json = new JsonObject();
-            json.addProperty("status", "OK");
-            return json.toString();
-        });
+
+        RequestProvider requestProvider = new RequestProvider();
+        requestProvider.gets();
+        FileProvider fileProvider = new FileProvider();
+        fileProvider.gets();
+
     }
 }
