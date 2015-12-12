49d6b0dc8ba2a87309300893c2af6f2fa672a535
marg <mksalawa@gmail.com>
2015-12-12 14:23:02 +0100

    status OK response to GET solutions/solution_id

--- colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/Hello.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.to.testerka;
 
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +11,11 @@ public class Hello {
     private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
 
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            LOGGER.info("Received GET request.");
-            return "hello from sparkjava.com";
+        get("/solutions/:solution_id", (req, res) -> {
+            LOGGER.info("Received GET request for solution {}.", req.params(":solution_id"));
+            JsonObject json = new JsonObject();
+            json.addProperty("status", "OK");
+            return json.toString();
         });
     }
 }
