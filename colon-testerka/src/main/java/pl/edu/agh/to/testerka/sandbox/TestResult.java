028ae879614484018896346444f6533abd3826b2
Maciej Warchoł <maciewar@gmail.com>
2015-12-15 16:52:22 +0100

    Add test result representation

---
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/sandbox/TestResult.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.to.testerka.sandbox;
+
+import com.google.gson.JsonObject;
+
+public class TestResult {
+    private TestResultStatus testResultStatus;
+    private long executionTimeMillis;
+
+    public TestResult(TestResultStatus testResultStatus, long executionTimeMillis) {
+        this.testResultStatus = testResultStatus;
+        this.executionTimeMillis = executionTimeMillis;
+    }
+
+    public JsonObject toJson() {
+        JsonObject testResultJson = new JsonObject();
+        testResultJson.addProperty("status", testResultStatus.toString());
+        testResultJson.addProperty("time", executionTimeMillis);
+        return testResultJson;
+    }
+}
---
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/sandbox/TestResultStatus.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.to.testerka.sandbox;
+
+public enum TestResultStatus {
+    OK("OK"),
+    TimeLimitExceeded("TLE"),
+    Answer("ANS"),
+    RuntimeError("RTE"),
+    Rejected("REJ");
+
+
+    private final String status;
+
+    TestResultStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+}
