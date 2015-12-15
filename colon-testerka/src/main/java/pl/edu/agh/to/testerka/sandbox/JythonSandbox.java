f5aed876f3e20b74f492a6b6565b608bb5fd501e
Maciej Warchoł <maciewar@gmail.com>
2015-12-15 17:00:07 +0100

    Implement the simplest possible sandbox

---
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/sandbox/JythonSandbox.java
@@ -0,0 +1,38 @@
+package pl.edu.agh.to.testerka.sandbox;
+
+import org.python.util.PythonInterpreter;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class JythonSandbox implements Sandbox {
+    @Override
+    public TestResult execute(String pythonCode, String input, String correctOutput) {
+        final Reader inputStream = new StringReader(input);
+        final Writer outputStream = new StringWriter();
+        PythonInterpreter interpreter = PythonInterpreterFactory.createPythonInterpreter(inputStream, outputStream);
+
+        // TODO: handle RTE and TLE statuses
+        long codeExecutionTime = measureExecutionTime(interpreter, pythonCode);
+        TestResultStatus testResultStatus = getTestResultStatus(outputStream, correctOutput);
+        return new TestResult(testResultStatus, codeExecutionTime);
+    }
+
+    private long measureExecutionTime(PythonInterpreter interpreter, String pythonCode) {
+        final long startTime = System.currentTimeMillis();
+        interpreter.exec(pythonCode);
+        final long finishTime = System.currentTimeMillis();
+        return finishTime - startTime;
+    }
+
+    private TestResultStatus getTestResultStatus(Writer outputStream, String correctOutput) {
+        final String codeOutput = outputStream.toString();
+        if (codeOutput.equals(correctOutput)) {
+            return TestResultStatus.OK;
+        } else {
+            return TestResultStatus.Answer;
+        }
+    }
+}
