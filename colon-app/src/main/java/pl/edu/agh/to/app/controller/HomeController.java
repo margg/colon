6c6d39d6c79637b942cb4f74a6d46ef5a803ef42
Stanisław Chmiela <sjchmiela@gmail.com>
2015-12-17 14:02:20 +0100

    Tried to list students from database. Somehow the student isn't mapped.

--- colon-app/src/main/java/pl/edu/agh/to/app/controller/HomeController.java
+++ colon-app/src/main/java/pl/edu/agh/to/app/controller/HomeController.java
@@ -1,17 +1,27 @@
 package pl.edu.agh.to.app.controller;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.edu.agh.to.app.model.Student;
+import pl.edu.agh.to.app.persistence.HibernateUtils;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
 public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json;charset=UTF-8", headers="Accept=*") @ResponseBody
-	public Student findAll() {
-		return new Student("aaa", "bbb", "usr1", "secret", null);
+	public List<Student> findAll() {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = session.beginTransaction();
+		List<Student> studentList = session.createQuery("from Student").list();
+		transaction.commit();
+		session.close();
+		return studentList;
 	}
 }
