532a9813733f25e73581c4ee5f4a8c91255d1d4e
Stanisław Chmiela <sjchmiela@gmail.com>
2015-12-17 12:57:04 +0100

    Fixed old initializer.

--- colon-app/src/main/java/pl/edu/agh/to/app/controller/HomeController.java
+++ colon-app/src/main/java/pl/edu/agh/to/app/controller/HomeController.java
@@ -2,6 +2,7 @@ package pl.edu.agh.to.app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.to.app.model.Student;
 
 @RestController
 public class HomeController {
@@ -9,6 +10,11 @@ public class HomeController {
     @RequestMapping("/api")
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/students")
+    public Student findAll() {
+        return new Student("aaa", "bbb", "usr1", "secret", null);
     }
 
 }
