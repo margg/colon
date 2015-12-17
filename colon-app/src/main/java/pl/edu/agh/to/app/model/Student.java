c0f134f8ea2e200f595005e7cd3c5c2fba422a76
Stanisław Chmiela <sjchmiela@gmail.com>
2015-12-17 12:56:25 +0100

    Added mappings for every concrete model class.

--- colon-app/src/main/java/pl/edu/agh/to/app/model/Student.java
+++ colon-app/src/main/java/pl/edu/agh/to/app/model/Student.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 public class Student extends User{
 
+    private long id;
+    private Group group;
     private List<Solution> solutions;
 
-    public Student(String firstName, String lastName, String username, List<Solution> solutions) {
-        super(firstName, lastName, username);
+    public Student(String firstName, String lastName, String username, String password, List<Solution> solutions) {
+        super(firstName, lastName, username, password);
         this.solutions = solutions;
     }
 
     public Student() {
         super();
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
     }
 
     public List<Solution> getSolutions() {
--- colon-app/src/main/java/pl/edu/agh/to/app/model/Task.java
+++ colon-app/src/main/java/pl/edu/agh/to/app/model/Task.java
@@ -10,7 +10,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Task {
-
+    private long id;
+    private String name;
+    private String description;
     @JsonBackReference
     private Teacher teacher;
     @JsonManagedReference
@@ -28,6 +30,30 @@ public class Task {
     }
 
     public Task() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @JsonBackReference
@@ -55,16 +81,19 @@ public class Task {
         this.timeLimit = timeLimit;
     }
 
+
+    // These should load test files from colon-files
     public String getTestInput() {
         return testInput;
     }
 
-    public void setTestInput(String testInput) {
-        this.testInput = testInput;
-    }
-
     public String getTestOutput() {
         return testOutput;
+    }
+
+    // These should save test files to colon-files
+    public void setTestInput(String testInput) {
+        this.testInput = testInput;
     }
 
     public void setTestOutput(String testOutput) {
