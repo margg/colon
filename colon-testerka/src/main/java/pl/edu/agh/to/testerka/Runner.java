62042f5e1f1fa058d17888f8e9807902796743bb
marg <mksalawa@gmail.com>
2016-01-02 22:25:09 +0100

    Add properties file, extract all configuration info

--- colon-testerka/src/main/java/pl/edu/agh/to/testerka/Runner.java
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/Runner.java
@@ -1,16 +1,42 @@
 package pl.edu.agh.to.testerka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.edu.agh.to.testerka.services.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Runner {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+    private static final String PROPERTIES_FILEPATH = "./colon-testerka/src/main/resources/application.properties";
+
     public static void main(String[] args) {
 
-        JDBCSaveResultService jdbcSaveResultService = new JDBCSaveResultService(new DBConnection());
-        // TODO: provide host address
-        FileContentProvider httpProvider = new HttpFileProvider("http://localhost:4567/mock/");
+        Properties properties = new Properties();
+        try(FileInputStream in = new FileInputStream(PROPERTIES_FILEPATH)) {
+            properties.load(in);
+        } catch (IOException e) {
+            LOGGER.error("Error while loading properties.", e);
+        }
+
+        String dbHostAddress = properties.getProperty("dbHostAddress");
+        String dbPort = properties.getProperty("dbPort");
+        String dbName = properties.getProperty("dbName");
+        String dbUsername = properties.getProperty("dbUsername");
+        String dbPassword = properties.getProperty("dbPassword");
+
+        String filerHostAddress = properties.getProperty("filerHostAddress");
+        String filerPort = properties.getProperty("filerPort");
+
+        JDBCSaveResultService jdbcSaveResultService =
+                new JDBCSaveResultService(new DBConnection(dbHostAddress, dbPort, dbName, dbUsername, dbPassword));
+        FileContentProvider httpProvider = new HttpFileProvider(filerHostAddress + ":" + filerPort);
         RunnerService runnerService = new RunnerService(jdbcSaveResultService, httpProvider);
-        StatusService statusService = new JDBCStatusService(new DBConnection());
+        StatusService statusService =
+                new JDBCStatusService(new DBConnection(dbHostAddress, dbPort, dbName, dbUsername, dbPassword));
         TesterHttpHandler testerHttpHandler = new TesterHttpHandler(runnerService, statusService);
         testerHttpHandler.setupTestersAPI();
 
--- colon-testerka/src/main/java/pl/edu/agh/to/testerka/services/DBConnection.java
+++ colon-testerka/src/main/java/pl/edu/agh/to/testerka/services/DBConnection.java
@@ -6,9 +6,23 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+    private String hostAddress;
+    private String port;
+    private String dbName;
+    private String username;
+    private String password;
+
+    public DBConnection(String hostAddress, String port, String dbName, String username, String password) {
+        this.hostAddress = hostAddress;
+        this.port = port;
+        this.dbName = dbName;
+        this.username = username;
+        this.password = password;
+    }
+
     public Connection getConnection() throws SQLException, ClassNotFoundException {
 
-        Class.forName("com.mysql.serviceImpl.Driver");
-        return DriverManager.getConnection("serviceImpl:mysql://localhost:5432/colondb","colon", "colon");
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://" + hostAddress + ":" + port + "/" + dbName, username, password);
     }
 }
